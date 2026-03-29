9594bfa2eff9a1c9123c273bd04c7eb821567389
BitravenS <malektabbabi77@gmail.com>
2026-03-29 07:35:57 +0100

    test: 78 bits now

--- scripts/patch_validator.py
+++ scripts/patch_validator.py
@@ -3,13 +3,12 @@ import sys
 import re
 import binascii
 
-TARGET_SIMILARITY_HASH = int("0x014206767F00DDEADBEEF", 16)
+TARGET_SIMILARITY_HASH = int("0x014206767F00DDEADBEEF", 16) & ((1 << 78) - 1)
 
-
-def get_74_bit_hash(text_block: str) -> int:
+def get_78_bit_hash(text_block: str) -> int:
     full_hash = hashlib.sha256(text_block.encode("utf-8")).digest()
     hash_int = int.from_bytes(full_hash[:11], byteorder="big")
-    return hash_int & ((1 << 74) - 1)
+    return hash_int & ((1 << 78) - 1)
 
 
 def validate_payload(filepath: str, expected_github_user: str) -> bool:
@@ -47,10 +46,10 @@ def validate_payload(filepath: str, expected_github_user: str) -> bool:
         )
         return False
 
-    h1 = get_74_bit_hash(block1)
-    h2 = get_74_bit_hash(block2)
-    h3 = get_74_bit_hash(block3)
-    h4 = get_74_bit_hash(block4)
+    h1 = get_78_bit_hash(block1)
+    h2 = get_78_bit_hash(block2)
+    h3 = get_78_bit_hash(block3)
+    h4 = get_78_bit_hash(block4)
 
     similarity_hash = h1 ^ h2 ^ h3 ^ h4
 
